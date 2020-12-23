e6e4342c8078f802f19c01d664245f4dc1a7d9ce
Karl Southern <karl@theangryangel.co.uk>
2020-12-23 16:41:40 +0000

    Precommit

--- mail_shortcode_menu/__manifest__.py
+++ mail_shortcode_menu/__manifest__.py
@@ -2,7 +2,8 @@
     "name": "mail_shortcode_menu",
     "website": "https://glodo.uk/",
     "author": "Glodo",
-    "summary": "Adds a menu to edit canned responses without livechat module being installed",
+    "summary": """Adds a menu to edit canned responses without livechat module
+being installed""",
     "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["mail", "base"],
